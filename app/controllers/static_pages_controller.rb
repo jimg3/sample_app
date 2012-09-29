42c3d27178e89818b86a33d31dacc6a591584b4a
jimg3 <jim.galley@gmail.com>
2012-09-29 12:46:21 -0400

    changed feed pagination to 10

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @micropost  = current_user.microposts.build
+#      WillPaginate::ViewHelpers.pagination_options[:inner_window] = 1
       @feed_items = current_user.feed.paginate(page: params[:page], per_page: 10)
     end
   end
